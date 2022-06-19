148ffb54867efdf20b9d501ba1cd0ddce47aa008
Kundan <kundan kamble>
2022-06-18 23:07:56 -0700

    [KUNDAN] Added Database connection program

---
+++ EmployeePayRollService/Exception.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetDatabaseCon
+{
+    public class Exceptions : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            ConnectionFailed
+        }
+        public Exceptions(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
+
+
---
+++ EmployeePayRollService/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NetDatabaseCon
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            program.EstablishConnection();
+            Console.WriteLine("Welcome to EmployeePayroll ADO.NET");
+            Console.WriteLine("Connection establised");
+        }
+        static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Payroll_Service;Integrated Security=SSPI";
+        static SqlConnection connection = new SqlConnection(connectionString);
+        public void EstablishConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Closed))
+            {
+                try
+                {
+                    connection.Open();
+
+                }
+                catch (Exception)
+                {
+                    throw new Exceptions(Exceptions.ExceptionType.ConnectionFailed, "Connection Failed.");
+                }
+            }
+            static void ClosedConnection()
+            {
+                if (connection != null && connection.State.Equals(ConnectionState.Open))
+                {
+                    try
+                    {
+                        connection.Close();
+                    }
+                    catch
+                    {
+                        throw new Exceptions(Exceptions.ExceptionType.ConnectionFailed, "Connection Failed.");
+                    }
+
+                }
+
+            }
+        }
+
+    }
+}
+
