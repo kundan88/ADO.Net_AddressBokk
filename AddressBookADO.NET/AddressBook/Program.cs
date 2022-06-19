a81f7db15e42d8628f150a784a4465441a3f7495
Kundan <kundan kamble>
2022-06-19 11:40:01 -0700

    [KUNDAN] Added Addressbook service program

---
+++ AddressBookADO.NET/AddressBook/Program.cs
@@ -0,0 +1,15 @@
+﻿using AddressBook;
+
+Console.WriteLine("AddressBook System");
+Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n");
+int op = Convert.ToInt16(Console.ReadLine());
+switch (op)
+{
+    case 1:
+        AddressBookData.Create_Database();
+        break;
+    default:
+        Console.WriteLine("Wrong Choice");
+        break;
+}
+
